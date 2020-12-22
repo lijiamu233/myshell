9ec684e5cab4aa18ceadec7e9f3bea48cf2e7fac
lijiamu233 <1127915415@qq.com>
2020-12-22 19:59:05 +0800

    Delete readline.h

--- readline.h
+++
@@ -1,8 +0,0 @@
-#include <cstdio>
-#include <cstdlib>
-#include <readline/readline.h>
-#include <readline/history.h>
-#ifndef SHELL_READLINE_H_
-#define SHELL_READLINE_H_
-char* myreadline(FILE *in);
-#endif
